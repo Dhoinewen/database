98c02a6a7b3a37073aea1026f8c63373e31e9782
Maxim <PogChamp1401@gmail.com>
2021-12-14 17:25:01 +0200

    aded swagger

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from data import build_report, create_racers_data
 from flask import Flask, Response
 from flask_restful import Api, Resource
 from helpers import convert_to_json, convert_to_xml, convert_racer_to_json
+from flasgger import Swagger
 
 app = Flask(__name__)
 api = Api(app)
+swagger = Swagger(app)
 
 
 class RacerList(Resource):
@@ -19,6 +21,9 @@ class RacerListXML(Resource):
 
 class Racer(Resource):
     def get(self, racer_abr):
+        """
+        file: app.yml
+        """
         return Response(convert_racer_to_json(create_racers_data()[racer_abr]), mimetype='application/json')
 
 
